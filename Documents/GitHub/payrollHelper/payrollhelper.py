670c5e2eeb2c49c13e897c27db947d31d5befa9a
Maya Dahlke <30633474+mdalhlke@users.noreply.github.com>
2023-01-08 15:28:52 -0800

    setting up flask

--- Documents/GitHub/payrollHelper/payrollhelper.py
+++ Documents/GitHub/payrollHelper/payrollhelper.py
@@ -1,46 +1,69 @@
+from flask import Flask, render_template, request, redirect
 import pandas as pd
 
-file = "testFile.xlsx"
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
-def createFile(spreadsheet, lessonType):
-    totalPay = 0
-    for index, row in spreadsheet.iterrows():
-        if row["Paid"] == "No":
-            totalPay += row["Commission"]
-    spreadsheet.loc[len(spreadsheet.index), "Package"] = "Total"
-    spreadsheet.loc[len(spreadsheet.index) - 1, "Total revenue"] = str(totalPay)
-    spreadsheet.to_excel(lessonType)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("payrollhelper.html")
 
-def sortCoachActivity(spreadsheet):
-        spreadsheet = spreadsheet[spreadsheet.Status != "cancel_on_time"]
-        privateLessons = spreadsheet.loc[spreadsheet["Type"] == "Lesson"]
-        clinics = spreadsheet.loc[spreadsheet["Type"] != "Lesson"]
-        createFile(privateLessons, "private.xlsx")
-        createFile(clinics, "clinics.xlsx")
+# this is the function that runs when the files are submitted
+@app.route("/data", methods=["GET", "POST"])
+def data():
+    if request.method == "POST":
+        file = request.files["file"]
+        
+        if file:
+            data = pd.read_excel(file, header=1)
+            df = data.sort_values("Teacher")
+            currName = None
+            for index, row in df.iterrows():
+                if row["Teacher"] != currName:
+                    currName = row["Teacher"]
+                    teacherInfo = df.loc[df["Teacher"] == currName]
+                    # sortCoachActivity(teacherInfo)
+            
+            return df.to_html()
+        
+        return redirect("/")
+
+
+
+# file = "testFile.xlsx"
+
+# def createFile(spreadsheet, lessonType):
+#     totalPay = 0
+#     for index, row in spreadsheet.iterrows():
+#         if row["Paid"] == "No":
+#             totalPay += row["Commission"]
+#     spreadsheet.loc[len(spreadsheet.index), "Package"] = "Total"
+#     spreadsheet.loc[len(spreadsheet.index) - 1, "Total revenue"] = str(totalPay)
+#     spreadsheet.to_excel(lessonType)
+
+# def sortCoachActivity(spreadsheet):
+#         spreadsheet = spreadsheet[spreadsheet.Status != "cancel_on_time"]
+#         privateLessons = spreadsheet.loc[spreadsheet["Type"] == "Lesson"]
+#         clinics = spreadsheet.loc[spreadsheet["Type"] != "Lesson"]
+#         createFile(privateLessons, "private.xlsx")
+#         createFile(clinics, "clinics.xlsx")
                
-def sortData(spreadsheet):
-    spreadsheet = pd.ExcelFile(spreadsheet)
-    sheets = list(spreadsheet.sheet_names)
-    coaches = []
-    for sheet in sheets:
-        df = spreadsheet.parse(sheet, header=1)
-        df = df.sort_values("Teacher") #look at later
-        currName = None
-        for index, row in df.iterrows():
-            if row["Teacher"] != currName:
-                currName = row["Teacher"]
-                teacherInfo = df.loc[df["Teacher"] == currName]
-                sortCoachActivity(teacherInfo)
-                break
-    return None
+# def sortData(spreadsheet):
+#     spreadsheet = pd.ExcelFile(spreadsheet)
+#     sheets = list(spreadsheet.sheet_names)
+#     coaches = []
+#     for sheet in sheets:
+#         df = spreadsheet.parse(sheet, header=1)
+#         df = df.sort_values("Teacher") #look at later
+#         currName = None
+#         for index, row in df.iterrows():
+#             if row["Teacher"] != currName:
+#                 currName = row["Teacher"]
+#                 teacherInfo = df.loc[df["Teacher"] == currName]
+#                 sortCoachActivity(teacherInfo)
+#                 break
+#     return None
 
 
     
-sortData(file)  
-
-
-
-
-
-
-
+# sortData(file)  
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-test
+# Payroll Helper
+
+### Setup
+Install [Flask](https://flask.palletsprojects.com/en/2.2.x/installation/)  
+  
+After activating environment run:  
+`export FLASK_APP=payrollhelper.py`  
+`export FLASK_DEBUG=1`  
+`flask run`  
+in the terminal
